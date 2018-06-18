1ca212c42c8a4a78e9afbf55c58d3be68ce6f38e
Iconslab <Truong.Nghiem@nau.edu>
2018-06-18 15:59:59 -0700

    Created a generic shape crazyflie

--- crazyflie_ws/src/iconslab/scripts/goalChangeGeneric.py
+++ crazyflie_ws/src/iconslab/scripts/goalChangeGeneric.py
@@ -9,27 +9,27 @@ from std_msgs.msg import Time
 def hover(x):
     return (0,0,1)
 
-def circleShape(radians):
-    return (math.cos(radians), math.sin(radians), 1)
+def circleShape(theta):
+    return (math.cos(theta), math.sin(theta), 1)
 
-def figure8(radians):
-    return (math.cos(radians),  math.sin(2*radians), 1)
+def figure8(theta):
+    return (math.cos(theta),  math.sin(2*theta), 1)
 
-shapeFuncs = {"circle":circleShape, "none":hover, "figure8":figure8}
+shapeFuncs = {"circle":circleShape, "hover":hover, "figure8":figure8}
 
 def shapeGoal():
     rospy.init_node('shapeGoal', anonymous=True)
 
-    loop_rate = 30
-
+    loop_rate = int(rospy.get_param("loop_rate", "30"))
     x_offset = float(rospy.get_param("x", "0"))
     y_offset = float(rospy.get_param("y", "0"))
     z_offset = float(rospy.get_param("z", "0"))
-    radians = float(rospy.get_param("radians", "0"))
+    theta = float(rospy.get_param("theta", "0"))
     hold_center = bool(rospy.get_param("hold", "True"))
-    radius = bool(rospy.get_param("radius", "True"))
+    radius = float(rospy.get_param("radius", "1"))
     delay = int(rospy.get_param("delay", "5000")) * loop_rate /1000
-    shape = rospy.get_param("shape", "none")
+    shape = rospy.get_param("shape", "hover")
+
     shapeFunc = shapeFuncs[shape]
 
     pubname = rospy.Publisher('goal', PoseStamped, queue_size=10)
@@ -48,12 +48,12 @@ def shapeGoal():
         pubname.publish(pos)
         rate.sleep()
     while not rospy.is_shutdown():
-        x,y,z = shapeFunc(radians)
+        x,y,z = shapeFunc(theta)
         pos.pose.position.x = x * radius + x_offset
         pos.pose.position.y = y * radius + y_offset
         pos.pose.position.z = z + z_offset
 	pubtime.publish(pos.header.stamp)
-        radians += 0.020
+        theta += 0.020
         pubname.publish(pos)
         rate.sleep()
 
